4f05ba5f03f15282ce9ccd2635f77ba788fb93b7
KaP1X0 <kacper.starzyczny13@gmail.com>
2024-06-05 20:57:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,15 @@
+// Theme Switch
+const toggleTheme = document.getElementById('toggle-theme');
+toggleTheme.addEventListener('change', () => {
+    document.body.classList.toggle('light-theme');
+});
+
+// Smooth Scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
-    });
-});
-const toggleBtn = document.querySelector('.toggle-btn');
-const nav = document.querySelector('nav');
-
-toggleBtn.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
-const heroText = document.querySelector('.hero h2');
-
-setInterval(() => {
-    heroText.classList.toggle('animate-text');
-}, 2000);
-document.querySelectorAll('.btn, .card').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-        item.classList.add('animate-hover');
-    });
-
-    item.addEventListener('mouseleave', () => {
-        item.classList.remove('animate-hover');
     });
 });
